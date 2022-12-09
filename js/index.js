356729e2f80059da5840b51059ec72335690794d
Pedro Kunz <56512310+pedrohkunz@users.noreply.github.com>
2022-12-09 20:41:13 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,16 +1,36 @@
-let celsius =  document.getElementById('inputcelsius')
-let fahrenheit = document.getElementById('outputfahrenheit')
+//Variáveis
+let input =  document.getElementById('input')
+let output = document.getElementById('output')
+var rem = "rem";
+let theme = new Boolean(false);
+let botao  = document.getElementById("btn");
+var img = document.querySelector("#img-btn");
 
+//Fazer o cálculo
 
 document.getElementById('converter').addEventListener("click", () => {
-    let resultado = inputcelsius.value * 1.8 + 32
-    console.log(resultado)
-
-    document.getElementById("outputfahrenheit").value = `${resultado}`
+    let resultado = input.value / 16
+    document.getElementById("output").value = `${resultado}${rem}`
+    //console.log(resultado)
 })
 
 document.getElementById('clear').addEventListener("click", () => {
-    document.getElementById("outputfahrenheit").value = `null`
-    document.getElementById("inputcelsius").value = `null`
+    document.getElementById("output").value = ``
+    document.getElementById("input").value = ``
 })
+
+//Alterar imagem do botão
+
+function clickbtn() {
+    theme = !theme
+    changeimg()
+}
+
+function changeimg(){
+    if(theme == false){
+        img.setAttribute('src', 'img/moon-fill.svg');
+    }else{
+        img.setAttribute('src', 'img/sun.svg');
+    }
+}
 
