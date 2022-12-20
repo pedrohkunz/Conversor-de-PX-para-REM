f305a7c7fa71865b6290efbb2563670bdf2a926d
Pedro Kunz <56512310+pedrohkunz@users.noreply.github.com>
2022-12-20 01:31:04 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,36 +1,25 @@
 //Variáveis
+
 let input =  document.getElementById('input')
 let output = document.getElementById('output')
-var rem = "rem";
 let theme = new Boolean(false);
 let botao  = document.getElementById("btn");
 var img = document.querySelector("#img-btn");
+var rem = "rem";
 
-//Fazer o cálculo
+//Realiza o Cálculo
 
 document.getElementById('converter').addEventListener("click", () => {
     let resultado = input.value / 16
     document.getElementById("output").value = `${resultado}${rem}`
-    //console.log(resultado)
+    
 })
+
+// Limpa o formulário
 
 document.getElementById('clear').addEventListener("click", () => {
     document.getElementById("output").value = ``
     document.getElementById("input").value = ``
 })
 
-//Alterar imagem do botão
-
-function clickbtn() {
-    theme = !theme
-    changeimg()
-}
-
-function changeimg(){
-    if(theme == false){
-        img.setAttribute('src', 'img/moon-fill.svg');
-    }else{
-        img.setAttribute('src', 'img/sun.svg');
-    }
-}
 
